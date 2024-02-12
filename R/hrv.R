d1cc779d103c9ae6ab4c61bb253134f9db6ce5d8
Maximilian Scholz <m0hawk@gmx.de>
2024-02-12 15:30:06 +0100

    Initial commit

---
+++ R/hrv.R
@@ -0,0 +1,253 @@
+#' Extract HR data from \code{FitFile} object
+#'
+#' @param fit_object An object of class \code{FitFile}, eg.
+#'   from \code{\link[FITfileR::readFitFile]{FITfileR::readFitFile}}
+#'
+#' @return A vector containing up to the first three minutes of heart rate data.
+#' @export
+get_HR <- function(fit_object){
+  HR <- records(fit_object)
+  if (all(class(HR) == "list")) {
+    HR <- HR[[which.max(sapply(HR, nrow))]]$heart_rate
+    HR <- HR[1:min(360, length(HR))]
+  } else {
+    HR <- HR$heart_rate[1:min(360, nrow(HR))]
+  }
+  return(HR)
+}
+
+#' Extract HRV data from \code{FitFile} object
+#'
+#' @param fit_object An object of class \code{FitFile}, eg.
+#'   from \code{\link[FITfileR::readFitFile]{FITfileR::readFitFile}}
+#'
+#' @return A vector containing up to the first three minutes of heart rate
+#'   variability data.
+#' @export
+get_HRV <- function(fit_object){
+  HRV <- filter(hrv(fit_object), time < 2)$time
+  return(HRV)
+}
+
+#' Calculate RR from HRV data.
+#'
+#' @param HRV A vector of HRV data (e.g. from \link{get_HRV})
+#' @param filter_factor Used to filter RR values that are 1.x times smaller or
+#'   larger than the last one as suspected false readings.
+#'
+#' @return A named list containing the full RR data and suspected RR data for
+#'   standing and lying down.
+#' @export
+get_RR <- function(HRV, filter_factor){
+  RR <- c(HRV[1])
+  for (i in 2:length(HRV)) {
+    if (HRV[i - 1] * (1 - filter_factor) < HRV[i] & HRV[i - 1] * (1 + filter_factor) > HRV[i]) {
+      RR <- append(RR, HRV[i])
+    }
+  }
+  RR <- data.frame(RR = RR, time = cumsum(RR))
+
+  RR$RR_ms <- RR$RR * 1000
+  RR$successive_differences <- numeric(length = nrow(RR))
+  for (i in 1:nrow(RR) - 1) {
+    RR$successive_differences[i] <- RR$RR_ms[i + 1] - RR$RR_ms[i]
+  }
+
+  return(list(
+    RR = RR,
+    laying_RR = RR[1:which.max(RR$time > 180) - 1, ],
+    standing_RR = RR[which.max(RR$time > 180):nrow(RR), ]
+  ))
+
+}
+
+#' Calculate HRV summaries for an orthostatic test
+#'
+#' @param fit_object An object of class \code{FitFile}, eg.
+#'   from \code{\link[FITfileR::readFitFile]{FITfileR::readFitFile}}
+#' @param filter_factor Used to filter RR values that are 1.x times smaller or
+#'   larger than the last one as suspected false readings.
+#'
+#' @return A named list of HRV summarie metrics.
+#' @export
+#'
+#' @importFrom FITfileR records hrv getMessagesByType
+#' @importFrom dplyr filter
+#' @importFrom clock as_date add_days get_hour
+hrv_metrics <- function(fit_object, filter_factor = 0.25) {
+  HR = get_HR(fit_object = fit_object)
+  HRV = get_HRV(fit_object = fit_object)
+  RR = get_RR(HRV = HRV, filter_factor)
+
+  metrics <- list(
+    laying_SDNN = round(sd(RR$laying_RR$RR_ms), digits = 2),
+    laying_rMSSD = round(sqrt(mean(RR$laying_RR$successive_differences^2))),
+    laying_mean_hr = round(mean(HR[1:180], na.rm = TRUE), digits = 2),
+    standing_SDNN = round(sd(RR$standing_RR$RR_ms), digits = 2),
+    standing_rMSSD = round(sqrt(mean(RR$standing_RR$successive_differences^2))),
+    standing_mean_hr = round(mean(HR[181:length(HR)], na.rm = TRUE), digits = 2),
+    standing_max_hr = max(HR),
+    date = as_date(getMessagesByType(fit_object, "session")$timestamp)
+  )
+  metrics$standing_time_to_mean_hr <- min(
+    which(HR <= metrics$standing_mean_hr)[
+      which(HR <= metrics$standing_mean_hr) > which.max(HR)
+      ]
+    ) -
+    which.max(HR)
+
+  metrics$morning <- ifelse(
+    (get_hour(getMessagesByType(fit_object, "session")$timestamp) > 4) &
+      (get_hour(getMessagesByType(fit_object, "session")$timestamp) < 13),
+    "Morning",
+    "Evening"
+  )
+
+
+  if (get_hour(getMessagesByType(fit_object, "session")$timestamp) < 4) {
+    metrics$date <- add_days(metrics$date, -1)
+  }
+
+  return(metrics)
+}
+
+
+#' Plot HRV summaries for a single orthostatic test fit file.
+#'
+#' @param fit_file_path Path to the fit file
+#' @param base "HR" if x axis should be heart rate or "RR" of x axis should be RR.
+#' @param filter_factor Used to filter RR values that are 1.x times smaller or
+#'   larger than the last one as suspected false readings.
+#'
+#' @return A ggplot2 plot object.
+#' @export
+#'
+#' @importFrom FITfileR readFitFile
+#' @importFrom dplyr '%>%'
+#' @importFrom ggplot2 ggplot geom_line geom_vline scale_x_continuous annotate aes xlab theme_bw
+hrv_plot <- function(fit_file_path, base = "RR", filter_factor = 0.25) {
+  fit_object = readFitFile(fit_file_path)
+
+  metrics <- hrv_metrics(fit_object = fit_object,
+                         filter_factor = filter_factor)
+
+  HR = get_HR(fit_object = fit_object)
+  RR = get_RR(HRV = get_HRV(fit_object = fit_object), filter_factor = filter_factor)
+  standing_RR = RR$standing_RR
+  laying_RR = RR$laying_RR
+  RR = RR$RR
+  HR = get_HR(fit_object = fit_object)
+
+  if (base == "RR") {
+    p <- RR %>%
+      ggplot(aes(x = time, y = RR)) +
+      geom_line() +
+      geom_vline(xintercept = 180, linetype = "dashed", color = "red") +
+      scale_x_continuous(limits = c(0, 360), breaks = c(0, 60, 120, 180, 240, 300, 360), expand = c(0, 0)) +
+      annotate("text",
+        color = "red", x = 80, y = mean(standing_RR$RR),
+        label = paste0("SDNN(ms): ", metrics$laying_SDNN)
+      ) +
+      annotate("text",
+        color = "red", x = 80, y = mean(standing_RR$RR) - 0.04,
+        label = paste0("rMSSD(ms): ", metrics$laying_rMSSD)
+      ) +
+      annotate("text",
+        color = "red", x = 80, y = mean(standing_RR$RR) - 0.08,
+        label = paste0("mean HR(bpm): ", metrics$laying_mean_hr)
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(laying_RR$RR) + 0.1,
+        label = paste0("SDNN(ms): ", metrics$standing_SDNN)
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(laying_RR$RR) + 0.06,
+        label = paste0("rMSSD(ms): ", metrics$standing_rMSSD)
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(laying_RR$RR) + 0.02,
+        label = paste0("mean HR(bpm): ", metrics$standing_mean_hr)
+      ) +
+      annotate("text", color = "red", x = 218, y = min(RR$RR), label = paste0("max HR(bpm): ", max(HR))) +
+      xlab("Time (s)") +
+      theme_bw()
+  } else if (base == "HR") {
+    p <- data.frame(HR = get_HR(fit_object = fit_object)) %>%
+      ggplot(aes(y = HR, x = 1:360)) +
+      geom_line() +
+      geom_vline(xintercept = 180, linetype = "dashed", color = "red") +
+      scale_x_continuous(limits = c(0, 360), breaks = c(0, 60, 120, 180, 240, 300, 360), expand = c(0, 0)) +
+      annotate("text",
+        color = "red", x = 80, y = mean(HR[180:360]) + 15,
+        label = paste0("SDNN(ms): ", round(sd(laying_RR$RR_ms), digits = 2))
+      ) +
+      annotate("text",
+        color = "red", x = 80, y = mean(HR[180:360]) + 11,
+        label = paste0("rMSSD(ms): ", round(sqrt(mean(laying_RR$successive_differences^2)), digits = 2))
+      ) +
+      annotate("text",
+        color = "red", x = 80, y = mean(HR[180:360]) + 7,
+        label = paste0("mean HR(bpm): ", round(mean(HR[1:180]), digits = 2))
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(HR[1:180]) + 5,
+        label = paste0("SDNN(ms): ", round(sd(standing_RR$RR_ms), digits = 2))
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(HR[1:180]) + 1,
+        label = paste0("rMSSD(ms): ", round(sqrt(mean(standing_RR$successive_differences^2)), digits = 2))
+      ) +
+      annotate("text",
+        color = "red", x = 280, y = mean(HR[1:180]) - 3,
+        label = paste0("mean HR(bpm): ", round(mean(HR[181:360]), digits = 2))
+      ) +
+      annotate("text", color = "red", x = 218, y = max(HR[10:360]), label = paste0("max HR(bpm): ", max(HR))) +
+      xlab("Time (s)") +
+      theme_bw()
+  } else {
+    stop("No proper base given. Must be 'RR' or 'HR'")
+  }
+  return(p)
+}
+
+#' Trend plots of HRV summaries for orthostatic tests
+#'
+#' @param fit_dir Path of directory containing fit files.
+#'
+#' @return A ggplot2 plot object.
+#' @export
+#' @importFrom dplyr '%>%' group_by summarise filter across bind_rows as_tibble
+#' @importFrom ggplot2 ggplot geom_point geom_line geom_hline facet_grid theme_bw aes
+hrv_trend_plot <- function(fit_dir) {
+  tests <- list.files(fit_dir, pattern = ".fit", full.names = TRUE)
+
+  metrics <- as_tibble(
+    do.call(
+      bind_rows,
+      lapply(tests,
+        function(x, ...) hrv_metrics(FITfileR::readFitFile(x), ...),
+        filter_factor = 0.175
+      )
+    )
+  ) %>%
+    tidyr::pivot_longer(
+      !c(date, morning),
+      names_to = c("position", "metric"),
+      names_pattern = "(.+?)_(.*)",
+      values_to = "value"
+    )
+
+  overall_means <- as.data.frame(metrics) %>%
+    group_by(across(c("morning", "position", "metric"))) %>%
+    summarise(mean = mean(value))
+
+  metrics %>%
+    ggplot(aes(x = date, y = value, color = position)) +
+    geom_point() +
+    geom_line() +
+    geom_hline(data = overall_means,
+               aes(yintercept = mean, color = position),
+               linetype = "dotted") +
+    facet_grid(metric ~ morning, scales = "free_y") +
+    theme_bw()
+}
